b969aea1b7a1ee29d357b7c0ffa56757be8c31df
Hoang Dung <hoangdung99er@gmail.com>
2021-12-19 17:47:20 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_ID_APP}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+      <Component {...pageProps} />)
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
